76d2d9042e8ceec5f35fdb982decc95a32449da9
Gabriel Jones <gabrieljones900@yahoo.com>
2022-05-09 11:18:01 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT
    Step: 64
    Utilize the window.alert() and document.write() methods.
    
    Assign string values to two variables and concatenate the variables together.
    
    Write an expression.

--- Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
@@ -1,4 +1,6 @@
 
-var A = "This is a string , and this \\ is a back slash " , B = "The Second String";
+var A = "This is a string , and this \" \\ \" is a back slash. " , B = "The Second String";
 
-document.write(3 + 3)
+window.alert(A+" "+B)
+
+document.write(3 + 3); //this adds 3 and 3 to print 6 if called
