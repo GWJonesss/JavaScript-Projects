6beb5bbad39c424e3ae86e164cc31b68d8786d37
Gabriel Jones <gabrieljones900@yahoo.com>
2022-05-10 09:30:53 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT
    Step: 69
    Write a function that uses the += operator
    
    Created an HTML element to call and display  function
    
    Added comments throughout the JavaScript explaining it

--- Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
@@ -1,6 +1,7 @@
 
-var A = "This is a string , and this \" \\ \" is a back slash. " , B = "The Second String";
+var A = "This is a string , and this \" \\ \" is a back slash. " , B = "Second String";
 
-window.alert(A+" "+B)
+window.alert(A+" "+B) //this adds the above var A and B, also this adds a space in the middle
+                      //by adding +" " between the two var. 
 
-document.write(3 + 3); //this adds 3 and 3 to print 6 if called
+document.write(3 + 3); //this adds 3 and 3 to print 6 if called in the html
