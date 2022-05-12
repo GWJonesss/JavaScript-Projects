aeffa063f7eb4e1d5b47ad539addcc41f4076ce5
Gabriel Jones <gabrieljones900@yahoo.com>
2022-05-12 15:17:26 -0700

    PROJECT5_COMPARISONS_TYPE_COERCION SUBMISSION ASSIGNMENT
    JAVASCRIPT COURSE Step: 126
    
    Utilize the document.write() method and typeof operator to display the data type of a variable of your choosing.
    
    Write an expression combining a string and a number.
    
    Write code utilizing the following operators: ==, ===, >, <, &&, ||, ! and display them in the browser.
    
    Add comments throughout your JavaScript explaining your code.

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,1 +1,47 @@
-document.write(typeof 5)
+
+//Typeof Operator//
+document.write(typeof 5 );
+document.write("<br>");
+
+//string and number//
+document.write(5+ " " + "This is five");
+document.write("<br>");
+
+//== operator//
+var b = 4;
+document.write(5==b);
+document.write("<br>");
+
+// === operator//
+
+var c = 5;
+document.write(5===c);
+document.write("<br>");
+
+//display > operator//
+var d = 4;
+document.write(5 > d);
+document.write("<br>");
+
+//display < operator//
+var e = 4;
+document.write(5 < e);
+document.write("<br>");
+
+//display && operator//
+var f = -2;
+var g = 5;
+document.write(f > 2 && g > 2); // false because both results ar not true
+document.write("<br>");
+
+//display || operator//
+var h = 7;
+var i = 3;
+document.write(i > 5 || h > 5);
+document.write("<br>");
+
+//display ! operator//
+var j = 2;
+var k = 8;
+document.write(! k < 5);
+document.write("<br>");
