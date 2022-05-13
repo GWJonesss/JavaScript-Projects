8c617a1f8d168f048892b72f4d7ab1e1e4497b5e
Gabriel Jones <gabrieljones900@yahoo.com>
2022-05-12 17:28:26 -0700

    updates

--- Basic-JavaScript-Projects/example.js
+++ Basic-JavaScript-Projects/example.js
@@ -1,5 +1,14 @@
-function my_Function() {
-    document.getElementById("test").innerHTML = isNaN('007"');
+function Vehicle (Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
 }
-
-document.write(2e310);
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+        document.getElementById("Key").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + " -colored " + Erik.Vehicle_Model + 
+        " manufactured in " + Erik.Vehicle_Year;
+}
