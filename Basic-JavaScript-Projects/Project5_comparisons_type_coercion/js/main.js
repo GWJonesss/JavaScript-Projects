0772bf23bf7b20b582d3ce1ae7e4ac3238beaa66
Gabriel Jones <gabrieljones900@yahoo.com>
2022-05-12 17:28:20 -0700

    updates

--- Basic-JavaScript-Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,47 +1,47 @@
 
 //Typeof Operator//
-document.write(typeof 5 );
+document.write(typeof 5 ); // output says this var type is  'number'
 document.write("<br>");
 
 //string and number//
-document.write(5+ " " + "This is five");
+document.write(5+ " " + "This is five"); // output ' 5 This is five//
 document.write("<br>");
 
 //== operator//
 var b = 4;
-document.write(5==b);
+document.write(5==b); // output false because 5 is not equal to b//
 document.write("<br>");
 
 // === operator//
 
 var c = 5;
-document.write(5===c);
+document.write(5===c); // output true because the two operands are equal//
 document.write("<br>");
 
 //display > operator//
 var d = 4;
-document.write(5 > d);
+document.write(5 > d); // output true because 5 is greater than d//
 document.write("<br>");
 
 //display < operator//
 var e = 4;
-document.write(5 < e);
+document.write(5 < e); // output false because 5 is not less than e//
 document.write("<br>");
 
 //display && operator//
 var f = -2;
 var g = 5;
-document.write(f > 2 && g > 2); // false because both results ar not true
+document.write(f > 2 && g > 2); // false because both results ar not true//
 document.write("<br>");
 
 //display || operator//
 var h = 7;
 var i = 3;
-document.write(i > 5 || h > 5);
+document.write(i > 5 || h > 5); //true because one of the statements is true//
 document.write("<br>");
 
 //display ! operator//
 var j = 2;
 var k = 8;
-document.write(! k < 5);
+document.write(! k < 5); //true because k is Not less than 5//
 document.write("<br>");
